068885746b91fddca97ddd0d03712b03d2dd185b
Anthony Oyathelemhi <anthony.oyathelemhi@deimos.co.za>
2023-05-21 14:27:21 +0100

    adjust styling

--- src/pages/app-home.ts
+++ src/pages/app-home.ts
@@ -14,21 +14,30 @@ export class AppHome extends LitElement {
       styles,
       css`
         .container {
-          display: grid;
-          min-height: 50vh;
-          grid-template-rows: auto 1fr;
+          height: 100vh;
+          height: 100dvh;
+          box-sizing: border-box;
           padding: 16px;
+          max-width: 460px;
+          margin: 0 auto;
+          background-color: #fff;
+          position: relative;
         }
 
         h1 {
-          font-size: 1.5rem;
+          font-size: 1.25rem;
           font-weight: 500;
+          text-align: center;
         }
 
         main {
           display: flex;
           flex-direction: column;
           justify-content: center;
+          position: absolute;
+          inset: 0;
+          margin: auto;
+          padding: 16px;
         }
 
         sl-card::part(footer) {
@@ -45,6 +54,20 @@ export class AppHome extends LitElement {
         .whatsapp-icon {
           margin-bottom: -2px;
           margin-left: 4px;
+        }
+
+        @media (min-width: 461px) {
+          .container {
+            margin-top: 32px;
+            margin-bottom: 32px;
+            height: calc(100vh - 64px);
+            height: calc(100dvh - 64px);
+            border-radius: 8px;
+            padding: 32px;
+          }
+          main {
+            padding: 32px;
+          }
         }
       `,
     ];
@@ -78,38 +101,36 @@ export class AppHome extends LitElement {
         <h1>Start a WhatsApp Chat with any Phone Number</h1>
 
         <main>
-          <div id="welcomeBar">
-            <sl-input
-              inputmode="tel"
-              label="Enter phone number"
-              size="large"
-              placeholder="2348012345678"
-              name="phone"
-              help-text="Include country code"
-              clearable
-            ></sl-input>
+          <sl-input
+            inputmode="tel"
+            label="Enter phone number"
+            size="large"
+            placeholder="2348012345678"
+            name="phone"
+            help-text="Include country code"
+            clearable
+          ></sl-input>
 
-            <sl-button
-              @click="${this.onChat}"
-              variant="success"
-              class="button"
-              size="large"
+          <sl-button
+            @click="${this.onChat}"
+            variant="success"
+            class="button"
+            size="large"
+          >
+            <span>Start WhatsApp Chat</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="whatsapp-icon"
+              viewBox="0 0 16 16"
             >
-              <span>Start WhatsApp Chat</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="whatsapp-icon"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"
-                />
-              </svg>
-            </sl-button>
-          </div>
+              <path
+                d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"
+              />
+            </svg>
+          </sl-button>
         </main>
       </div>
     `;
